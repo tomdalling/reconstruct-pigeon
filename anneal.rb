8c98173727d0450e7d1e9ba0588417b3470156f1
Tom Dalling <tom@tomdalling.com>
2016-10-22 14:35:15 +1100

    Random beziers

--- anneal.rb
+++ anneal.rb
@@ -5,10 +5,11 @@ require 'rmagick'
 class Window < Gosu::Window
   def initialize
     @needs_redraw = true
-    @target_image = Magick::Image::read('alan3.jpg').first
+    @target_image = Magick::Image::read('alan4.png').first
     @accum = Magick::Image.new(@target_image.columns, @target_image.rows) { self.background_color = 'none'}
     @output = Gosu::Image.new(@accum)
     @running = false
+    @debug = false
     @max_draw = @target_image.columns * 0.2
     @coord_queue = []
 
@@ -36,6 +37,7 @@ class Window < Gosu::Window
   def button_down(button)
     case button
     when Gosu::KbSpace then @running = !@running
+    when Gosu::KbD then @debug = !@debug
     when Gosu::KbEscape, Gosu::KbQ then close
     end
   end
@@ -43,8 +45,12 @@ class Window < Gosu::Window
   def draw
     @output.insert(@accum, 0, 0)
     @output.draw(0, 0, 0)
-    @font.draw("Comps per sec: #{@comps_per_sec}", 0, 0, 0, 1, 1, Gosu::Color::RED)
-    @font.draw("Max size: #{@max_draw}", 0, 20, 0, 1, 1)
+
+    if @debug
+      @font.draw("Comps per sec: #{@comps_per_sec}", 0, 0, 0, 1, 1, Gosu::Color::RED)
+      @font.draw("Max size: #{@max_draw}", 0, 20, 0, 1, 1)
+    end
+
     @needs_redraw = false
   end
 
@@ -92,20 +98,51 @@ class Window < Gosu::Window
     w = rand(1.0...@max_draw) || 1
     h = rand(1.0...@max_draw) || 1
 
-    circle = Magick::Draw.new
-    circle.fill(color)
-    circle.ellipse(x, y, w, h, 0, 360)
-    circle.draw(img)
+    draw = Magick::Draw.new
+    draw.translate(x, y)
+    draw.rotate(rand(0..360))
+    draw_random_bezier(draw, w, h, color)
+    draw.draw(img)
 
     rx = Float(w) / 2.0
     ry = Float(h) / 2.0
     [(x - rx).floor, (y - ry).floor, 2*rx.ceil, 2*ry.ceil]
   end
 
+  def draw_random_elipse(draw, w, h, color)
+    draw.fill(color)
+    draw.ellipse(0, 0, w, h, 0, 360)
+  end
+
+  def draw_random_line(draw, w, h, color)
+    thickness = [1.0, (w + h) / 20.0].max
+    draw.stroke(color)
+    draw.stroke_width(thickness)
+    draw.line(-w, -h, w, h)
+  end
+
+  def draw_random_bezier(draw, w, h, color)
+    thickness = [1.0, (w + h) / 5.0].max
+    minx = -w
+    maxx = w
+    miny = -h
+    maxy = h
+
+    draw.stroke(color)
+    draw.fill('none')
+    draw.stroke_width(thickness)
+    draw.bezier(
+      -w, -h,
+      rand(minx..maxx), rand(miny..maxy),
+      rand(minx..maxx), rand(miny..maxy),
+      w, h
+    )
+  end
+
   def enqueue_coords!
     @max_draw = (@max_draw <= 3 ? 3 : @max_draw * 0.90)
 
-    step = Float(@max_draw) * 0.75
+    step = Float(@max_draw) / 2.0
 
     0.step(by: step, to: width).flat_map do |x|
       0.step(by: step, to: height).map do |y|
@@ -119,7 +156,7 @@ class Window < Gosu::Window
 
     @coord_queue.sort_by!(&:last)
     @coord_queue.reverse!
-    @coord_queue.pop((@coord_queue.size * 0.7).floor)
+    @coord_queue.pop((@coord_queue.size * 0.85).floor)
   end
 
   def color_diff(c1, c2)
